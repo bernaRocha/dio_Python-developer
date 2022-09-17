dec1d6d3fa214a63f8e6d10555f2d72c52a2b778
Bernardo <mrochabernardo@gmail.com>
2022-09-17 06:34:27 -0300

    revisão de listas

---
+++ estrutura-de-dados/001_listas.py
@@ -0,0 +1,12 @@
+from os import system
+system('clear')
+
+frutas = []
+frutas = ['Laranja', 'Maça', 'Uva', 'Banana']
+
+letras = list('python')
+print(letras[0]) # p
+print(letras[-1]) # n
+
+tamanho = list(range(10))
+print(tamanho) # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
